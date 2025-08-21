e3714c67724d1b2df69528bf1a8e7e94b80db224
adarsh0311 <adarshghai6@gmail.com>
2025-08-21 01:07:38 -0700

    add insert item and get item functions

--- src/main/java/aws/dynamodb/DynamoDbDemo.java
+++ src/main/java/aws/dynamodb/DynamoDbDemo.java
@@ -5,17 +5,28 @@ import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class DynamoDbDemo {
     private static final Logger log = LoggerFactory.getLogger(DynamoDbDemo.class);
-    static DynamoDbClient client = DynamoDBConnection.createClient();
+    DynamoDbClient client = DynamoDBConnection.createClient();
+
 
     public static void main(String[] args) {
-        createTable();
+        DynamoDbDemo demo = new DynamoDbDemo();
+        //demo.createTable();
+
+        String userID = UUID.randomUUID().toString();
+        demo.insertItem("Users", userID); //or you can pass 123 as userid for now
+
+        demo.getItem("Users", userID);
 
     }
 
 
-    public static void createTable() {
+    public void createTable() {
          CreateTableRequest request = CreateTableRequest.builder()
                 .tableName("Users")
                 .keySchema(KeySchemaElement.builder()
@@ -33,7 +44,36 @@ public class DynamoDbDemo {
                 .build();
 
         CreateTableResponse response = client.createTable(request);
-        log.info(response.toString());
         log.info("Table created");
+        log.info(response.toString());
+
+    }
+
+
+    public void insertItem(String tableName, String userId) {
+        Map<String, AttributeValue> item = Map.of(
+                "userId", AttributeValue.builder().s(userId).build(),
+                "name", AttributeValue.builder().s("abc").build(),
+                "age", AttributeValue.builder().n("123").build()
+        );
+        PutItemRequest request = PutItemRequest.builder()
+                .tableName(tableName)
+                .item(item)
+                .build();
+
+        PutItemResponse response = client.putItem(request);
+        log.info(response.toString());
+
+    }
+
+    public GetItemResponse getItem(String tableName, String userId) {
+        GetItemRequest request = GetItemRequest.builder()
+                .tableName(tableName)
+                .key(Map.of("userId", AttributeValue.builder().s(userId).build()))
+                .build();
+
+        GetItemResponse response = client.getItem(request);
+        log.info(response.toString());
+        return response;
     }
 }
